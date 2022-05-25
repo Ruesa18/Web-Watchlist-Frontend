cc2301dc17722f144f7a8db01945e3bf0598b01d
Sandro Rüfenacht <sandro.ruefenacht0@gmail.com>
2022-05-25 18:59:34 +0200

    start waiting on submit of login form

--- src/component/Login.tsx
+++ src/component/Login.tsx
@@ -1,18 +1,38 @@
 import React from "react";
 import '../css/Login.css';
 
-export class Login extends React.Component {
+type LoginState = {
+    email: string,
+    password: string
+}
+
+export class Login extends React.Component<{}, LoginState> {
+
+    constructor(props: Readonly<Object> | Object) {
+        super(props);
+
+        this.state = {
+            email: "",
+            password: ""
+        };
+    }
+
     render() {
         return (
             <div id="login">
                 <h1>Login</h1>
 
-                <form>
-                    <input type="email" name="email" placeholder="Email" />
-                    <input type="password" name="password" placeholder="Password" />
-                    <input type="submit" value="Login" />
+                <form onSubmit={(e) => {e.preventDefault(); return false}}>
+                    <input type="email" name="email" value={this.state.email} onChange={(e) => {this.setState({email: e.target.value})}} placeholder="Email" />
+                    <input type="password" name="password" value={this.state.password} onChange={(e) => {this.setState({password: e.target.value})}} placeholder="Password" />
+                    <button onClick={() => {this.loginHandler()}}>Login</button>
                 </form>
             </div>
         );
+    }
+
+    loginHandler() {
+        console.log("Email: " + this.state.email, "Password: " + this.state.password);
+        //TODO implement login
     }
 }
